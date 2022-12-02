2c45adcc54482a7b0f79e3475615e374548789a2
leegwichan <cnddkscndgus@naver.com>
2022-12-02 20:38:26 +0900

    fix(OutputView) : printStations 의 맨 위 소개줄 출력 부분 수정
    - 출력 요구 사항을 반영하여 맨 위 소개줄 추가

--- src/main/java/subway/view/output/OutputView.java
+++ src/main/java/subway/view/output/OutputView.java
@@ -1,11 +1,13 @@
 package subway.view.output;
 
 import subway.domain.Station;
+import subway.dto.StationDto;
 import java.util.List;
 
 public class OutputView {
 
     private static final String INFO_PREFIX = "[INFO] ";
+    private static final String STATION_PRINT_TEXT = "## 역 목록";
 
     public void printView(View view) {
         print(view.getText());
@@ -15,13 +17,14 @@ public class OutputView {
         printResult(operation.getText());
     }
 
-    public void printStations(List<Station> stations) {
-        for (Station station : stations) {
+    public void printStations(List<StationDto> stations) {
+        print(STATION_PRINT_TEXT);
+        for (StationDto station : stations) {
             printStation(station);
         }
     }
 
-    private void printStation(Station station) {
+    private void printStation(StationDto station) {
         printResult(station.getName());
     }
 
