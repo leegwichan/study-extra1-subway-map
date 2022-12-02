1e5183b5c52316ce9a631d1779b48612f40c3ce2
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2022-12-02 14:59:00 +0900

    feature(OutputView) : 역 목록 출력 기능 구현

--- src/main/java/subway/view/output/OutputView.java
+++ src/main/java/subway/view/output/OutputView.java
@@ -1,5 +1,8 @@
 package subway.view.output;
 
+import subway.domain.Station;
+import java.util.List;
+
 public class OutputView {
 
     private static final String INFO_PREFIX = "[INFO] ";
@@ -10,6 +13,16 @@ public class OutputView {
 
     private void printDoSuccess(Operation operation) {
         printResult(operation.getText());
+    }
+
+    public void printStations(List<Station> stations) {
+        for (Station station : stations) {
+            printStation(station);
+        }
+    }
+
+    private void printStation(Station station) {
+        printResult(station.getName());
     }
 
     private void printResult(String string) {
