4c6fbb4ef071d53cb556798f29a379074559e2a6
leegwichan <cnddkscndgus@naver.com>
2022-12-02 20:48:07 +0900

    feature(OutputView) : 노선 목록 출력 기능 구현

--- src/main/java/subway/view/output/OutputView.java
+++ src/main/java/subway/view/output/OutputView.java
@@ -1,6 +1,7 @@
 package subway.view.output;
 
 import subway.domain.Station;
+import subway.dto.LineDto;
 import subway.dto.StationDto;
 import java.util.List;
 
@@ -8,6 +9,7 @@ public class OutputView {
 
     private static final String INFO_PREFIX = "[INFO] ";
     private static final String STATION_PRINT_TEXT = "## 역 목록";
+    private static final String LINE_PRINT_TEXT = "## 노선 목록";
 
     public void printView(View view) {
         print(view.getText());
@@ -24,8 +26,19 @@ public class OutputView {
         }
     }
 
+    public void printLines(List<LineDto> lines) {
+        print(LINE_PRINT_TEXT);
+        for (LineDto lineDto : lines) {
+            printLine(lineDto);
+        }
+    }
+
     private void printStation(StationDto station) {
         printResult(station.getName());
+    }
+
+    private void printLine(LineDto lineDto) {
+        printResult(lineDto.getName());
     }
 
     private void printResult(String string) {
